456970bdd8af5e40e6cb247dc521d199c0bdbfa5
Will Hodgkinson <willhodgkinson5@gmail.com>
2022-08-01 22:28:01 +0100

    boolean test passes

--- src/app.js
+++ src/app.js
@@ -14,6 +14,8 @@ const {
 
 const { add, subtract, multiply, divide, remainder } = require('../lib/numbers');
 
+const { negate, truthiness, isOdd, startsWith } = require('../lib/booleans');
+
 app.get('/strings/hello/:string', (req, res) => {
   res.json({ result: sayHello(req.params.string) });
 });
@@ -89,4 +91,29 @@ app.post('/numbers/remainder', (req, res) => {
     res.status(200).json({ result: remainder(req.body.a, req.body.b) });
   }
 });
+
+app.post('/booleans/negate', (req, res) => {
+  res.json({ result: negate(req.body.value) });
+});
+
+app.post('/booleans/truthiness', (req, res) => {
+  res.json({ result: truthiness(req.body.value) });
+});
+
+app.get('/booleans/is-odd/:number', (req, res) => {
+  if (Number.isNaN(parseInt(req.params.number, 10))) {
+    res.status(400).json({ error: 'Parameter must be a number.' });
+  } else {
+    res.json({ result: isOdd(req.params.number) });
+  }
+});
+
+app.get('/booleans/:string/starts-with/:character', (req, res) => {
+  if (req.params.character.length > 1) {
+    res.status(400).json({ error: 'Parameter "character" must be a single character.' });
+  } else {
+    res.json({ result: startsWith(req.params.character, req.params.string) });
+  }
+});
+
 module.exports = app;
